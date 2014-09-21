c833553898928707d4d00413f0bc1fe259f937b6
Shashwat Anand <shashwat.anand@gmail.com>
2014-09-21 11:09:35 +0530

    Added editor class

---
+++ src/firstgefpro/editor/GraphicalEditorActionBarContributor.java
@@ -0,0 +1,12 @@
+package firstgefpro.editor;
+
+import org.eclipse.ui.part.EditorActionBarContributor;
+
+public class GraphicalEditorActionBarContributor extends
+		EditorActionBarContributor {
+
+	public GraphicalEditorActionBarContributor() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
