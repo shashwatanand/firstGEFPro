4b55649c5d3aa5445a64b5e3e7facbd956ad4923
Shashwat Anand <shashwat.anand@gmail.com>
2014-09-21 11:16:05 +0530

    Modified - Made the editor Graphical editor

--- src/firstgefpro/editor/GraphicalEditor.java
+++ src/firstgefpro/editor/GraphicalEditor.java
@@ -1,59 +1,42 @@
 package firstgefpro.editor;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorSite;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.part.EditorPart;
+import org.eclipse.gef.DefaultEditDomain;
+import org.eclipse.gef.palette.PaletteRoot;
+import org.eclipse.gef.ui.parts.GraphicalEditorWithPalette;
 
-public class GraphicalEditor extends EditorPart {
+/**
+ * GraphicalEditor.java
+ * @author Shashwat Anand
+ * @version $Id: $
+ * @since Sept 21, 2014
+ */
+public class GraphicalEditor extends GraphicalEditorWithPalette {
+	public static final String ID = "firstGEFPro.graphicalEditor";
 
+	/**
+	 * Constructor
+	 */
 	public GraphicalEditor() {
-		// TODO Auto-generated constructor stub
+		setEditDomain(new DefaultEditDomain(this));
+	}
+
+
+	@Override
+	protected void initializeGraphicalViewer() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
 	public void doSave(IProgressMonitor monitor) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
-	public void doSaveAs() {
+	protected PaletteRoot getPaletteRoot() {
 		// TODO Auto-generated method stub
-
+		return null;
 	}
-
-	@Override
-	public void init(IEditorSite site, IEditorInput input)
-			throws PartInitException {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean isDirty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isSaveAsAllowed() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void createPartControl(Composite parent) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void setFocus() {
-		// TODO Auto-generated method stub
-
-	}
-
 }
