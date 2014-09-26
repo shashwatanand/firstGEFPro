77e73577e00eeae95a748f7a870fa66dbd5999d8
Shashwat Anand <shashwat.anand@gmail.com>
2014-09-26 23:50:27 +0530

    Adding abstract edit part

---
+++ src/firstgefpro/part/AbstractEditPart.java
@@ -0,0 +1,26 @@
+package firstgefpro.part;
+
+import java.beans.PropertyChangeListener;
+import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
+import firstgefpro.model.Node;
+
+/**
+ * AbstractEditPart.java
+ * 
+ * @author Shashwat Anand
+ * @version $Id: $
+ * @since Sept 27, 2014
+ */
+public abstract class AbstractEditPart extends AbstractGraphicalEditPart implements PropertyChangeListener {
+	@Override
+	public void activate() {
+		super.activate();
+		((Node) getModel()).addPropertyChangeListener(this);
+	}
+	
+	@Override
+	public void deactivate() {
+		super.deactivate();
+		((Node) getModel()).removePropertyChangeListener(this);
+	}
+}
