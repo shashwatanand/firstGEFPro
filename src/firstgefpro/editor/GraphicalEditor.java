378f7aa8a24fd073d80579882a95ea3ed127afc3
Shashwat Anand <shashwat.anand@gmail.com>
2014-09-28 13:38:53 +0530

    Modified

--- src/firstgefpro/editor/GraphicalEditor.java
+++ src/firstgefpro/editor/GraphicalEditor.java
@@ -1,6 +1,7 @@
 package firstgefpro.editor;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.ContextMenuProvider;
 import org.eclipse.gef.DefaultEditDomain;
 import org.eclipse.gef.EditPart;
@@ -14,12 +15,9 @@ import org.eclipse.gef.palette.PaletteSeparator;
 import org.eclipse.gef.palette.SelectionToolEntry;
 import org.eclipse.gef.ui.parts.GraphicalEditorWithPalette;
 
-
-
-
-
 import firstgefpro.command.NodeCreationFactory;
 import firstgefpro.model.Employee;
+import firstgefpro.model.Entreprise;
 import firstgefpro.model.Service;
 import firstgefpro.part.AppEditPartFactory;
 
@@ -49,8 +47,48 @@ public class GraphicalEditor extends GraphicalEditorWithPalette {
 
 
 	private EditPart createEntreprise() {
-		// TODO Auto-generated method stub
-		return null;
+	    Entreprise psyEntreprise = new Entreprise();
+	    psyEntreprise.setName("Psykokwak Entreprise");
+	    psyEntreprise.setAddress("Quelque part sur terre");
+	    psyEntreprise.setCapital(100000);
+
+	    Service comptaService = new Service();
+	    comptaService.setName("Compta");
+	    comptaService.setEtage(2);
+	    comptaService.setLayout(new Rectangle(30, 50, 250, 150));
+	    Employee employeCat = new Employee();
+	    employeCat.setName("Debroua");
+	    employeCat.setPrenom("Cat");
+	    employeCat.setLayout(new Rectangle(25, 40, 60, 40));
+	    comptaService.addChild(employeCat);
+	    Employee employeJyce = new Employee();
+	    employeJyce.setName("Psykokwak");
+	    employeJyce.setPrenom("Jyce");
+	    employeJyce.setLayout(new Rectangle(100, 60, 60, 40));
+	    comptaService.addChild(employeJyce);
+	    Employee employeEva = new Employee();
+	    employeEva.setName("Longoria");
+	    employeEva.setPrenom("Eva");
+	    employeEva.setLayout(new Rectangle(180, 90, 60, 40));
+	    comptaService.addChild(employeEva);
+	    psyEntreprise.addChild(comptaService);
+	    Service rhService = new Service();
+	    rhService.setName("Ressources Humaine");
+	    rhService.setEtage(1);
+	    rhService.setLayout(new Rectangle(220, 230, 250, 150));
+	    Employee employePaul = new Employee();
+	    employePaul.setName("Dupond");
+	    employePaul.setPrenom("Paul");
+	    employePaul.setLayout(new Rectangle(40, 70, 60, 40));
+	    rhService.addChild(employePaul);
+	    Employee employeEric = new Employee();
+	    employeEric.setName("Durand");
+	    employeEric.setPrenom("Eric");
+	    employeEric.setLayout(new Rectangle(170, 100, 60, 40));
+	    rhService.addChild(employeEric);
+	    psyEntreprise.addChild(rhService);
+
+	    return psyEntreprise;
 	}
 
 	@Override
