a564eb13719f7d7ecf8d1b0f15e651e5a59fe4d8
Shashwat Anand <shashwat.anand@gmail.com>
2014-10-01 18:29:15 +0530

    Updated Node model class

--- src/firstgefpro/model/Node.java
+++ src/firstgefpro/model/Node.java
@@ -8,18 +8,25 @@ import org.eclipse.draw2d.geometry.Rectangle;
 
 /**
  * Node.java
+ * 
  * @author Shashwat Anand
  * @version $Id: $
  * @since Sept 25, 2014
  */
 public class Node {
+	public static final String PROPERTY_ADD = "NodeAddChild";
+
+	public static final String PROPERTY_LAYOUT = "NodeLayout";
+
 	private PropertyChangeSupport listeners;
-	
+
 	private Rectangle layout;
 
 	private List<Node> children;
 
 	private String name;
+
+	private Node parent;
 
 	public String getName() {
 		return name;
@@ -39,5 +46,41 @@ public class Node {
 
 	public void removePropertyChangeListener(PropertyChangeListener listener) {
 		listeners.removePropertyChangeListener(listener);
+	}
+
+	public boolean addChild(Node child) {
+		boolean b = this.children.add(child);
+		if (b) {
+			child.setParent(this);
+			getListeners().firePropertyChange(PROPERTY_ADD, null, child);
+		}
+		return b;
+	}
+
+	public boolean removeChild(Node child) {
+		return this.children.remove(child);
+	}
+
+	public List<Node> getChildrenArray() {
+		return this.children;
+	}
+
+	public void setParent(Node parent) {
+		this.parent = parent;
+	}
+
+	public Node getParent() {
+		return this.parent;
+	}
+
+	public void setLayout(Rectangle newLayout) {
+		Rectangle oldLayout = this.layout;
+		this.layout = newLayout;
+		getListeners()
+				.firePropertyChange(PROPERTY_LAYOUT, oldLayout, newLayout);
+	}
+
+	public Rectangle getLayout() {
+		return this.layout;
 	}
 }
